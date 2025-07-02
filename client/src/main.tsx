26e880300c501fb5b28fe727f0fc57e668b4fd53
Conrad Joel Mbuya <joelconrad277@gmail.com>
2025-07-02 17:10:20 +0300

    pwa changes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,6 +6,17 @@ import { BrowserRouter } from "react-router";
 import { Provider } from "react-redux";
 import store, { persistor } from "./redux/store.ts";
 import { PersistGate } from "redux-persist/integration/react";
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // Automatically refresh to load the new version
+    updateSW(true); // This line forces update
+  },
+  onOfflineReady() {
+    console.log("App is ready to work offline");
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
