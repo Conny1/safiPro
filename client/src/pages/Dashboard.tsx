2f8d6b6b223a8a7498f2904e6fc481f7dd0f3426
Conrad Joel Mbuya <joelconrad277@gmail.com>
2025-07-02 17:27:35 +0300

    ui chages

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -207,7 +207,7 @@ const Dashboard = () => {
                 className="flex items-center gap-2 bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-gray-700 transition"
               >
                 <PlusCircle size={18} />
-                New brach
+                New branch
               </button>
               <button
                 onClick={() => setlisbranchesModal(true)}
