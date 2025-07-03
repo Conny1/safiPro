9942fd95c415ad4760f2972c260583b97b5edfd8
Conrad Joel Mbuya <joelconrad277@gmail.com>
2025-07-03 14:20:12 +0300

    cors changes

--- api/src/app.js
+++ api/src/app.js
@@ -9,7 +9,20 @@ const appRoute = require("./routes");
 dotenv.config();
 app.use(express.json());
 
-app.use(cors());
+const allowedOrigins = [process.env.CLIENT_URL, process.env.PAYSTACK_BASE_URL];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(helmet());
 app.use(morgan("dev"));
 
