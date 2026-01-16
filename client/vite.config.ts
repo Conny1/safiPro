96f30f0919a31c9d6e4d9d05b41a7abee54317eb
Conny1 <joelconrad277@gmail.com>
2026-01-16 20:21:51 +0300

    offline mode config changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,9 +13,6 @@ export default defineConfig({
       filename:"service-worker.js",
       registerType: "autoUpdate", // handles update checking
       devOptions:{enabled:true, type:"module" },
-      workbox:{
-        navigateFallback:"/index.html"
-      },
       includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
       manifest: {
         id: "/dashboard",
