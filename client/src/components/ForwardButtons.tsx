f1bf61b0ec2c353a617e5e090b2a927efc53595f
Conny1 <joelconrad277@gmail.com>
2026-02-17 20:09:23 +0300

    phone no bug fix

--- client/src/components/ForwardButtons.tsx
+++ client/src/components/ForwardButtons.tsx
@@ -3,26 +3,25 @@ type Props = {
   phone_number: string;
 };
 const ForwardButtons = ({ message, phone_number }: Props) => {
-  const phoneNumber = phone_number.startsWith("254")
-    ? phone_number
-    : `254${phone_number}`;
+  const type = phone_number.startsWith("254") || phone_number.startsWith("+254");
+  const phoneNumber = type ? phone_number : `254${phone_number}`;
 
   return (
-    <div className="flex items-center gap-3  flex-wrap ">
+    <div className="flex flex-wrap items-center gap-3 ">
       <a
         href={`sms:+${phoneNumber}?&body=${encodeURIComponent(message)}`}
-        className="bg-blue-600 text-white px-4 py-2 rounded block text-center"
+        className="block px-4 py-2 text-center text-white bg-blue-600 rounded"
       >
         Send SMS Notification
       </a>
 
       <a
         href={`https://wa.me/${phoneNumber}?text=${encodeURIComponent(
-          message
+          message,
         )}`}
         target="_blank"
         rel="noopener noreferrer"
-        className="bg-green-600 text-white px-4 py-2 rounded block text-center"
+        className="block px-4 py-2 text-center text-white bg-green-600 rounded"
       >
         Send WhatsApp Notification
       </a>
